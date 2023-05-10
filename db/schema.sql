a58a2024f779b1264b95d69ef9be165911f5617e
valerierobles <roblesv08@gmail.com>
2023-05-10 17:47:42 -0500

    used activities for seeds & schema

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS trackemplyee_db;
+
+CREATE DATABASE trackemplyee_db;
+
+USE trackemplyee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE trackemplyee_db;
+
+INSERT INTO department (name) VALUES ('FINANCE');
+INSERT INTO department (name) VALUES ('LEGAL');
+INSERT INTO DEPARTMENT (title, salary, department_id) VALUES ('Lawyer', 100000, 2);
+INSERT INTO DEPARTMENT (title, salary, department_id) VALUES ('Accountant', 80000, 1);
+INSERT INTO DEPARTMENT (title, salary, department_id) VALUES ('Accountant', 90000, 1);
+INSERT INTO DEPARTMENT (title, salary, department_id) VALUES ('Legal Team Lead', 250000, 2);
